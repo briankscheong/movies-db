3b908ab926b37a2812725f8d2ae8cd7834f634fd
Cheong, Brian Kean Shiong <brcheong@iu.edu>
2024-10-25 12:24:15 -0400

    fixed ui styling for mobile

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -9,7 +9,9 @@ export default function Search({ placeholder, mobile }: { placeholder: string, m
     const pathname = usePathname();
     const { replace } = useRouter();
     const maxSearchWidth = mobile ? "max-w-32" : "max-w-52";
-    const searchClassName = `${maxSearchWidth} peer block rounded-md hover:border hover:border-gray-200 py-[9px] pl-8 pr-4 text-sm text-white outline-2 placeholder:text-gray-300 bg-gradient-to-r from-indigo-900 to-blue-900`;
+    const leftPadding = mobile ? "pl-4" : "pl-8";
+    const searchPlaceholder = mobile ? "search..." : placeholder;
+    const searchClassName = `${maxSearchWidth} ${leftPadding} peer block rounded-md hover:border hover:border-gray-200 py-[9px] pr-4 text-sm text-white outline-2 placeholder:text-gray-300 bg-gradient-to-r from-indigo-900 to-blue-900`;
 
 
     function handleSearch(term: string | undefined) {
@@ -31,7 +33,7 @@ export default function Search({ placeholder, mobile }: { placeholder: string, m
                 <MagnifyingGlassIcon className="absolute left-2 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-300 peer-focus:text-gray-200" />
                 <input
                     className={searchClassName}
-                    placeholder={placeholder}
+                    placeholder={searchPlaceholder}
                     onChange={(e) => {
                         handleSearch(e.target.value);
                     }}
