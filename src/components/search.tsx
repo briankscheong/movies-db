50dd3a8bd1601e847e8dda227c73757e201c51b3
Cheong, Brian Kean Shiong <brcheong@iu.edu>
2024-10-25 02:20:07 -0400

    remove mobile search component version

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -20,41 +20,41 @@ export default function Search({ placeholder, mobile }: { placeholder: string, m
     }
     
     return (
-        mobile ? (
-            <div>
-                <div className="relative flex flex-1 flex-shrink-0 pointer-events-none">
-                    <label htmlFor="search" className="sr-only">
-                        Search
-                    </label>
-                    <MagnifyingGlassIcon className="absolute left-2 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-300 peer-focus:text-gray-200" />
-                    <input
-                        className="max-w-30 peer block rounded-md border border-gray-200 py-[9px] pl-8 pr-4 text-sm text-white outline-2 placeholder:text-gray-400 bg-gradient-to-r from-indigo-900 to-blue-900"
-                        placeholder="search..."
-                        onChange={(e) => {
-                            handleSearch(e.target.value);
-                        }}
-                        defaultValue={searchParams.get('query')?.toString()}
-                    />
-                </div>
+        // mobile ? (
+        //     <div>
+        //         <div className="relative flex flex-1 flex-shrink-0 pointer-events-none">
+        //             <label htmlFor="search" className="sr-only">
+        //                 Search
+        //             </label>
+        //             <MagnifyingGlassIcon className="absolute left-2 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-300 peer-focus:text-gray-200" />
+        //             <input
+        //                 className="max-w-30 peer block rounded-md border border-gray-200 py-[9px] pl-8 pr-4 text-sm text-white outline-2 placeholder:text-gray-400 bg-gradient-to-r from-indigo-900 to-blue-900"
+        //                 placeholder="search..."
+        //                 onChange={(e) => {
+        //                     handleSearch(e.target.value);
+        //                 }}
+        //                 defaultValue={searchParams.get('query')?.toString()}
+        //             />
+        //         </div>
+        //     </div>
+        // ) : (
+        <div className="mx-5">
+            <div className="relative flex flex-1 flex-shrink-0">
+                <label htmlFor="search" className="sr-only">
+                    Search
+                </label>
+                <MagnifyingGlassIcon className="absolute left-2 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-300 peer-focus:text-gray-200" />
+                <input
+                    className="max-w-40 peer block rounded-md border border-gray-200 py-[9px] pl-8 pr-4 text-sm text-white outline-2 placeholder:text-gray-300 bg-gradient-to-r from-indigo-900 to-blue-900"
+                    placeholder={placeholder}
+                    onChange={(e) => {
+                        handleSearch(e.target.value);
+                    }}
+                    defaultValue={searchParams.get('query')?.toString()}
+                    // value={searchParams.get('query')?.toString()}
+                />
             </div>
-        ) : (
-            <div className="mx-5">
-                <div className="relative flex flex-1 flex-shrink-0">
-                    <label htmlFor="search" className="sr-only">
-                        Search
-                    </label>
-                    <MagnifyingGlassIcon className="absolute left-2 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-300 peer-focus:text-gray-200" />
-                    <input
-                        className="max-w-40 peer block rounded-md border border-gray-200 py-[9px] pl-8 pr-4 text-sm text-white outline-2 placeholder:text-gray-300 bg-gradient-to-r from-indigo-900 to-blue-900"
-                        placeholder={placeholder}
-                        onChange={(e) => {
-                            handleSearch(e.target.value);
-                        }}
-                        defaultValue={searchParams.get('query')?.toString()}
-                        // value={searchParams.get('query')?.toString()}
-                    />
-                </div>
-            </div>
-        )
+        </div>
+        // )
     );
 }
