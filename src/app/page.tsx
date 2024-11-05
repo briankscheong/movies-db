82b534b4586e192af6ec21207bdaf6ea352831c6
Cheong, Brian Kean Shiong <brcheong@iu.edu>
2024-11-05 01:22:37 -0500

    minor styling adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,8 +88,8 @@ export default function Home() {
   // });
 
   return (
-    <div className="w-screen h-screen text-center px-5 items-center justify-center bg-gradient-to-b from-gray-950 to-black">
-      <Image className="w-screen h-screen opacity-30" src={Wallpaper} alt="Movies wallpaper" layout="fill" objectFit="cover"></Image>
+    <div className="w-screen h-screen text-center pt-20 px-5 items-center justify-center bg-gradient-to-b from-gray-950 to-black">
+      <Image className="opacity-30" src={Wallpaper} alt="Movies wallpaper" layout="fill" objectFit="cover"></Image>
       <GibberishText text="Movies.DB" className="text-4xl font-bold text-white font-mono" />
       <br />
       <div className="w-full absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-row justify-center items-center">
